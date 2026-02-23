115b49052d478a3ce00eae75d8a1e03e2218d638
JÚLIA TIBURCIO MARTINS <026071@fatecararas.com>
2026-02-23 07:44:42 -0300

    nova versao

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+
+<body style="background-color: blue;"> 
+    
+    <h1 style="color: black;">TCC</h1> 
+
     <h1>TCC<h1>
 </body>
 </html>
